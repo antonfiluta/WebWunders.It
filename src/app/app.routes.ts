e791c05cc11c23e61ba7752af369c126a394ef27
antonfiluta <antonfiluta2.0@mail.ru>
2024-07-26 16:00:44 +0300

    feat: build fund

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+    {path: 'contact', component: ContactPageComponent},
+    {path: 'projects', component: ProjectsPageComponent},
+];
